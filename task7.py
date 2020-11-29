56c976dcc8eaf791d5d92f37cf212f609d3c95ff
Charishma3 <66492385+Charishma3@users.noreply.github.com>
2020-11-30 01:14:58 +0530

    Add files via upload

---
+++ task7.py
@@ -0,0 +1,27 @@
+#1.
+#file:module.py
+my_list=[1,2,3]
+
+#file:task7Q1
+#importing the module
+
+import module
+module.my_list.append(4)
+print(module.my_list)
+
+#2.
+#installed pandas package using pip
+
+#3.
+import random
+print("Random number", random.randint(0, 100))
+
+
+#4.
+
+import sys
+print(sys.path)
+
+
+#5.
+#installed and unistalled package using pip
